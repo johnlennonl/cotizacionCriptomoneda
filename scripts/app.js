50bf8d601d2ced88ab8bbb6c681a88c36e4b3920
johnlennonl <jojopow2410@gmail.com>
2024-05-30 18:15:52 -0400

    segunda prueba

--- scripts/app.js
+++ scripts/app.js
@@ -1,113 +1,125 @@
-// Obtener los precios de Binance// Obtener los precios de Binance
-fetch('https://api.binance.com/api/v3/ticker/price')
-.then(response => response.json())
-.then(binanceData => {
-  const binanceMap = {};
-  binanceData.forEach(crypto => {
-    binanceMap[crypto.symbol] = {
-      price: parseFloat(crypto.price).toFixed(3),
-      hourChange: null,
-      dayChange: null
-    };
-  });
-
-  fetch('https://api.binance.com/api/v3/ticker/24hr')
-    .then(response => response.json())
-    .then(binanceChangeData => {
-      let totalChangePercent = 0;
-      let totalCryptos = 0;
-
-      binanceChangeData.forEach(crypto => {
-        if (binanceMap[crypto.symbol]) {
-          binanceMap[crypto.symbol].hourChange = parseFloat(crypto.priceChangePercent);
-          binanceMap[crypto.symbol].dayChange = parseFloat(crypto.priceChangePercent);
-
-          totalChangePercent += binanceMap[crypto.symbol].dayChange;
-          totalCryptos++;
-        }
-      });
-
-      const averageChangePercent = totalChangePercent / totalCryptos;
-      const marketChangeHeader = document.getElementById('market-change');
-      marketChangeHeader.innerHTML = `En las últimas 24 h, el mercado ${averageChangePercent >= 0 ? 'subió' : 'bajó'} un <span style="color: ${averageChangePercent >= 0 ? 'green' : 'red'};">${averageChangePercent.toFixed(2)}%</span>`;
-
-      return fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-        .then(response => response.json())
-        .then(geckoData => {
-          const tableBody = document.querySelector('#crypto-table tbody');
-          tableBody.innerHTML = '';
-
-          geckoData.forEach(crypto => {
-            const binanceSymbol = crypto.symbol.toUpperCase() + 'USDT';
-            const binancePriceData = binanceMap[binanceSymbol];
-
-            if (binancePriceData) {
-              const row = tableBody.insertRow();
-              const cell1 = row.insertCell(0);
-              const cell2 = row.insertCell(1);
-              const cell3 = row.insertCell(2);
-              const cell4 = row.insertCell(3);
-              const cell5 = row.insertCell(4);
-
-              const img = document.createElement('img');
-              img.src = crypto.image;
-              img.width = 15;
-              img.height = 15;
-
-              const symbolText = document.createTextNode(crypto.symbol.toUpperCase());
-              const nameText = document.createTextNode(` (${crypto.name})`);
-
-              cell1.appendChild(img);
-              cell1.appendChild(symbolText);
-              cell1.appendChild(nameText);
-              cell1.style.marginRight = '5px';
-
-              cell2.textContent = `$${binancePriceData.price}`;
-              cell3.textContent = `$${crypto.market_cap.toLocaleString()}`;
-              cell3.classList.add('desktop-only');
-              cell4.textContent = `${binancePriceData.hourChange}%`;
-              cell4.style.color = binancePriceData.hourChange >= 0 ? 'green' : 'red';
-              cell5.textContent = `${binancePriceData.dayChange}%`;
-              cell5.style.color = binancePriceData.dayChange >= 0 ? 'green' : 'red';
-
-              row.setAttribute('data-symbol', crypto.symbol.toUpperCase());
-              row.setAttribute('data-name', crypto.name.toUpperCase());
-            }
-          });
-        });
+// Obtener los precios de las criptomonedas desde Binance
+fetch("https://api.binance.com/api/v3/ticker/price")
+  .then((response) => response.json()) // Parsear la respuesta JSON
+  .then((binanceData) => {
+    const binanceMap = {}; // Crear un objeto para mapear los datos de Binance
+    binanceData.forEach((crypto) => {
+      binanceMap[crypto.symbol] = {
+        price: parseFloat(crypto.price).toFixed(3), // Guardar el precio con tres decimales
+        hourChange: null,
+        dayChange: null,
+      };
     });
-})
-.catch(error => console.error('Error al obtener los datos de la API de Binance o CoinGecko:', error));
 
-document.getElementById('search').addEventListener('input', function () {
-const searchValue = this.value.toUpperCase();
-const table = document.getElementById('crypto-table');
-const rows = table.getElementsByTagName('tr');
+    // Obtener cambios porcentuales de las últimas 24 horas desde Binance
+    fetch("https://api.binance.com/api/v3/ticker/24hr")
+      .then((response) => response.json()) // Parsear la respuesta JSON
+      .then((binanceChangeData) => {
+        let totalChangePercent = 0; // Inicializar la suma de los cambios porcentuales
+        let totalCryptos = 0; // Inicializar el contador de criptomonedas
 
-for (let i = 1; i < rows.length; i++) {
-  const symbol = rows[i].getAttribute('data-symbol');
-  const name = rows[i].getAttribute('data-name');
-  if (symbol.indexOf(searchValue) > -1 || name.indexOf(searchValue) > -1) {
-    rows[i].style.display = '';
-    rows[i].classList.add('highlight');
-  } else {
-    rows[i].style.display = 'none';
-    rows[i].classList.remove('highlight');
+        binanceChangeData.forEach((crypto) => {
+          if (binanceMap[crypto.symbol]) {
+            binanceMap[crypto.symbol].hourChange = parseFloat(
+              crypto.priceChangePercent
+            );
+            binanceMap[crypto.symbol].dayChange = parseFloat(
+              crypto.priceChangePercent
+            );
+
+            totalChangePercent += binanceMap[crypto.symbol].dayChange; // Acumular el cambio porcentual diario
+            totalCryptos++; // Incrementar el contador de criptomonedas
+          }
+        });
+
+        const averageChangePercent = totalChangePercent / totalCryptos; // Calcular el cambio porcentual promedio
+        const marketChangeHeader = document.getElementById("market-change"); // Obtener el elemento del encabezado del cambio de mercado
+        marketChangeHeader.innerHTML = `En las últimas 24 h, el mercado ${
+          averageChangePercent >= 0 ? "subió" : "bajó"
+        } un <span style="color: ${
+          averageChangePercent >= 0 ? "green" : "red"
+        };">${averageChangePercent.toFixed(2)}%</span>`;
+
+        // Obtener datos de CoinGecko para más información sobre las criptomonedas
+        return fetch(
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        )
+          .then((response) => response.json()) // Parsear la respuesta JSON
+          .then((geckoData) => {
+            const tableBody = document.querySelector("#crypto-table tbody"); // Obtener el cuerpo de la tabla
+            tableBody.innerHTML = ""; // Limpiar el contenido actual de la tabla
+
+            geckoData.forEach((crypto) => {
+              const binanceSymbol = crypto.symbol.toUpperCase() + "USDT"; // Construir el símbolo Binance
+              const binancePriceData = binanceMap[binanceSymbol]; // Obtener los datos de precio de Binance
+
+              if (binancePriceData) {
+                const row = tableBody.insertRow(); // Insertar una nueva fila en la tabla
+                const cell1 = row.insertCell(0); // Insertar celdas en la fila
+                const cell2 = row.insertCell(1);
+                const cell3 = row.insertCell(2);
+                const cell4 = row.insertCell(3);
+                const cell5 = row.insertCell(4);
+
+                // Crear y añadir imagen de la criptomoneda
+                const img = document.createElement("img");
+                img.src = crypto.image;
+                img.width = 15;
+                img.height = 15;
+
+                // Crear y añadir el texto del símbolo y nombre de la criptomoneda
+                const symbolText = document.createTextNode(
+                  crypto.symbol.toUpperCase()
+                );
+                const nameText = document.createTextNode(` (${crypto.name})`);
+
+                cell1.appendChild(img); // Añadir la imagen a la celda
+                cell1.appendChild(symbolText); // Añadir el símbolo a la celda
+                cell1.appendChild(nameText); // Añadir el nombre a la celda
+                cell1.style.marginRight = "5px"; // Añadir margen derecho a la celda
+
+                // Añadir los datos de precio y cambios porcentuales a las celdas correspondientes
+                cell2.textContent = `$${binancePriceData.price}`;
+                cell3.textContent = `$${crypto.market_cap.toLocaleString()}`;
+                cell3.classList.add("desktop-only"); // Añadir clase CSS para ocultar en dispositivos móviles
+                cell4.textContent = `${binancePriceData.hourChange}%`;
+                cell4.style.color =
+                  binancePriceData.hourChange >= 0 ? "green" : "red"; // Cambiar color según el valor
+                cell5.textContent = `${binancePriceData.dayChange}%`;
+                cell5.style.color =
+                  binancePriceData.dayChange >= 0 ? "green" : "red"; // Cambiar color según el valor
+
+                // Añadir atributos de datos para búsqueda y filtrado
+                row.setAttribute("data-symbol", crypto.symbol.toUpperCase());
+                row.setAttribute("data-name", crypto.name.toUpperCase());
+              }
+            });
+          });
+      });
+  })
+  .catch((error) =>
+    console.error(
+      "Error al obtener los datos de la API de Binance o CoinGecko:",
+      error
+    )
+  );
+
+// Añadir evento de búsqueda
+document.getElementById("search").addEventListener("input", function () {
+  const searchValue = this.value.toUpperCase(); // Obtener el valor de búsqueda y convertirlo a mayúsculas
+  const table = document.getElementById("crypto-table"); // Obtener la tabla de criptomonedas
+  const rows = table.getElementsByTagName("tr"); // Obtener todas las filas de la tabla
+
+  for (let i = 1; i < rows.length; i++) {
+    const symbol = rows[i].getAttribute("data-symbol"); // Obtener el símbolo de la criptomoneda
+    const name = rows[i].getAttribute("data-name"); // Obtener el nombre de la criptomoneda
+    // Mostrar u ocultar filas según el valor de búsqueda
+    if (symbol.indexOf(searchValue) > -1 || name.indexOf(searchValue) > -1) {
+      rows[i].style.display = ""; // Mostrar la fila
+      rows[i].classList.add("highlight"); // Añadir clase de resaltado
+    } else {
+      rows[i].style.display = "none"; // Ocultar la fila
+      rows[i].classList.remove("highlight"); // Eliminar clase de resaltado
+    }
   }
-}
-});
-
-// Alternar vistas
-document.getElementById('full-view').addEventListener('click', function () {
-const columns = document.querySelectorAll('.desktop-only');
-columns.forEach(column => {
-  column.classList.remove('hidden-column');
-});
-});
-
-document.getElementById('mobile-view').addEventListener('click', function () {
-const columns = document.querySelectorAll('.desktop-only');
-columns.forEach(column => {
-  column.classList.add('hidden-column');
-});
 });
--- scripts/crypto.js
+++ scripts/crypto.js
@@ -1,58 +1,66 @@
+// Array de criptomonedas con sus símbolos y nombres
 const cryptocurrencies = [
-    { symbol: "BTC", name: "Bitcoin" },
-    { symbol: "ETH", name: "Ethereum" },
-    { symbol: "LTC", name: "Litecoin" },
-    { symbol: "BNB", name: "BNB" },
-    { symbol: "SOL", name: "Solana" },
-    { symbol: "DOGE", name: "Dogecoin" },
-    { symbol: "SHIB", name: "SHIBA INU" },
-    { symbol: "XRP", name: "XRP" },
-    { symbol: "ADA", name: "Cardano" },
-    { symbol: "XLM", name: "Stellar" },
-    { symbol: "AVAX", name: "Avalanche" },
-    { symbol: "ATOM", name: "Cosmos" }
-  ];
-  
-  // Obtener elementos del DOM
-  const seleccionarCriptomoneda = document.getElementById("seleccionarCriptomoneda");
-  const montoCriptomoneda = document.getElementById("montoCriptomoneda");
-  const botonCalcular = document.getElementById("botonCalcular");
-  const mostrarValor = document.getElementById("mostrarValor");
-  
-  // Llenar opciones de selección de criptomonedas
-  cryptocurrencies.forEach((crypto) => {
-    const option = document.createElement("option");
-    option.value = crypto.symbol;
-    option.text = crypto.name;
-    seleccionarCriptomoneda.appendChild(option);
-  });
-  
-  // Función para calcular el valor de una criptomoneda en USD
-  const calcularValorCriptomoneda = (symbol, amount) => {
-    const url = `https://api.binance.com/api/v3/ticker/price?symbol=${symbol}USDT`;
-    
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((data) => {
-        const precioUSD = parseFloat(data.price);
-        const valorUSD = amount * precioUSD;
-        mostrarValor.innerHTML = `<p class="mostrar_resultado">El valor de ${amount} ${symbol} es de $${valorUSD.toFixed(2)} USD.</p>`;
-      })
-      .catch((error) => {
-        mostrarValor.innerHTML = "Ha ocurrido un error al obtener los datos.";
-        console.error("Error:", error);
-      });
-  };
-  
-  // Agregar evento al botón de calcular
-  botonCalcular.addEventListener("click", () => {
-    const symbol = seleccionarCriptomoneda.value;
-    const amount = parseFloat(montoCriptomoneda.value);
-  
-    if (isNaN(amount) || amount <= 0) {
-      mostrarValor.innerHTML = "Por favor, ingrese un monto válido.";
-      return;
-    }
-  
-    calcularValorCriptomoneda(symbol, amount);
-  });
+  { symbol: "BTC", name: "Bitcoin" },
+  { symbol: "ETH", name: "Ethereum" },
+  { symbol: "LTC", name: "Litecoin" },
+  { symbol: "BNB", name: "BNB" },
+  { symbol: "SOL", name: "Solana" },
+  { symbol: "DOGE", name: "Dogecoin" },
+  { symbol: "SHIB", name: "SHIBA INU" },
+  { symbol: "XRP", name: "XRP" },
+  { symbol: "ADA", name: "Cardano" },
+  { symbol: "XLM", name: "Stellar" },
+  { symbol: "AVAX", name: "Avalanche" },
+  { symbol: "ATOM", name: "Cosmos" }
+];
+
+// Obtener elementos del DOM (Document Object Model)
+const seleccionarCriptomoneda = document.getElementById("seleccionarCriptomoneda");
+const montoCriptomoneda = document.getElementById("montoCriptomoneda");
+const botonCalcular = document.getElementById("botonCalcular");
+const mostrarValor = document.getElementById("mostrarValor");
+
+// Llenar el elemento de selección con las opciones de criptomonedas
+cryptocurrencies.forEach((crypto) => {
+  // Crear un nuevo elemento <option> para cada criptomoneda
+  const option = document.createElement("option");
+  option.value = crypto.symbol; // Establecer el valor del <option> al símbolo de la criptomoneda
+  option.text = crypto.name; // Establecer el texto del <option> al nombre de la criptomoneda
+  seleccionarCriptomoneda.appendChild(option); // Añadir el <option> al elemento de selección
+});
+
+// Función para calcular el valor de una criptomoneda en USD
+const calcularValorCriptomoneda = (symbol, amount) => {
+  // Construir la URL de la API de Binance para obtener el precio de la criptomoneda
+  const url = `https://api.binance.com/api/v3/ticker/price?symbol=${symbol}USDT`;
+
+  // Realizar una solicitud fetch a la API de Binance
+  fetch(url)
+    .then((resp) => resp.json()) // Parsear la respuesta JSON
+    .then((data) => {
+      const precioUSD = parseFloat(data.price); // Obtener el precio en USD y convertirlo a número
+      const valorUSD = amount * precioUSD; // Calcular el valor total en USD
+      // Mostrar el resultado en el DOM
+      mostrarValor.innerHTML = `<p class="mostrar_resultado">El valor de ${amount} ${symbol} es de $${valorUSD.toFixed(2)} USD.</p>`;
+    })
+    .catch((error) => {
+      // Manejar errores de la solicitud
+      mostrarValor.innerHTML = "Ha ocurrido un error al obtener los datos.";
+      console.error("Error:", error); // Imprimir el error en la consola para depuración
+    });
+};
+
+// Agregar un evento al botón de calcular
+botonCalcular.addEventListener("click", () => {
+  const symbol = seleccionarCriptomoneda.value; // Obtener el símbolo de la criptomoneda seleccionada
+  const amount = parseFloat(montoCriptomoneda.value); // Obtener y convertir el monto ingresado a número
+
+  // Validar que el monto ingresado sea un número positivo
+  if (isNaN(amount) || amount <= 0) {
+    mostrarValor.innerHTML = "Por favor, ingrese un monto válido."; // Mostrar mensaje de error en el DOM
+    return; // Salir de la función si el monto no es válido
+  }
+
+  // Llamar a la función para calcular el valor de la criptomoneda
+  calcularValorCriptomoneda(symbol, amount);
+});
